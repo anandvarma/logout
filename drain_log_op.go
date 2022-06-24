6b61bf27297136b6484d903bdc2907f34efe0aae
Anand Varma <achekur@ncsu.edu>
2022-06-23 23:26:04 -0400

    Remove port prefix from public URL

--- drain_log_op.go
+++ drain_log_op.go
@@ -32,7 +32,7 @@ func (op *DrainLogOp) Start() {
 	defer op.Finish()
 
 	// Greet.
-	url := fmt.Sprintf("http://%s:%s/view?token=%x\n", PUBLIC_IP, WEB_PORT, op.tokenNum)
+	url := fmt.Sprintf("http://%s/view?token=%x\n", PUBLIC_IP, op.tokenNum)
 	op.conn.Write([]byte(url))
 	if PRINT_QR_CODE {
 		qrterminal.GenerateHalfBlock(url, qrterminal.L, op.conn)
