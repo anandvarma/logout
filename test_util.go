ee19de1e42ce982357241a4ddc7858aff7f443aa
Anand Varma <anand.pyro@gmail.com>
2023-07-30 01:41:19 -0500

    Added test coverage for ring buf

---
+++ test_util.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"reflect"
+	"runtime/debug"
+	"testing"
+)
+
+func checkEq[V any](t *testing.T, a V, b V) {
+	if !reflect.DeepEqual(a, b) {
+		t.Fatalf("got:%v, want:%v \n%s", a, b, debug.Stack())
+	}
+}
